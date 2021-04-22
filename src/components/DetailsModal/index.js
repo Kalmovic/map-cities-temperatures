a7bab0d52afcfa5f8808d3978b171068f253341c
Kalmovic <joseph.kaltenecker@gmail.com>
2021-04-22 16:25:13 -0300

    details modal component

---
+++ src/components/DetailsModal/index.js
@@ -0,0 +1,44 @@
+import React, { memo } from "react";
+import ReactModal from "react-modal";
+import closeImg from "../../assets/close.svg";
+import { toCelsius } from "../../utils/convertTemperature";
+import {
+  AllTempInfoWrapper,
+  CityName,
+  TypeOfTemp,
+  Temp,
+  TempWrapper,
+} from "./styles";
+
+function DetailsModal({ isOpen, onRequestClose, cityDetails }) {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+      appElement={document.getElementById("root")}
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close modal" />
+      </button>
+      <CityName>{cityDetails.name}</CityName>
+      <AllTempInfoWrapper>
+        <TempWrapper>
+          <Temp>{toCelsius(cityDetails.temp_min)}º C</Temp>
+          <TypeOfTemp>Minimum</TypeOfTemp>
+        </TempWrapper>
+        <TempWrapper>
+          <Temp>{toCelsius(cityDetails.temp_max)}º C</Temp>
+          <TypeOfTemp>Maximum</TypeOfTemp>
+        </TempWrapper>
+      </AllTempInfoWrapper>
+    </ReactModal>
+  );
+}
+
+export default memo(DetailsModal);
---
+++ src/components/DetailsModal/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const CityName = styled.h2`
+  text-align: center;
+  font-size: 20px;
+`;
+export const AllTempInfoWrapper = styled.section`
+  display: flex;
+  justify-content: space-around;
+`;
+export const TempWrapper = styled.section`
+  margin: 20px 0 20px 0;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Temp = styled.h1`
+  font-size: 45px;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const TypeOfTemp = styled.h3`
+  font-size: 16px;
+  text-align: center;
+`;
